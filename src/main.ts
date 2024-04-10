09128263b689328ea6b8080992c6819f5c3d6d79
Laura <laura@upschool.fr>
2024-04-10 17:27:58 +0200

    test cors

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,7 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors({
-    origin: ['https://mundi-bellum.onrender.com', 'https://mundi-bellum.lauramathieu.com/', 'https://www.mundi-bellum.lauramathieu.com/', '*'],
-    credentials: true,
-  });
+  app.enableCors();
 
   await app.listen(3007);
 }
