44e2cfa158027b9de28899b43622480e26b86156
Laura <laura@upschool.fr>
2024-04-10 16:25:02 +0200

    test auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,16 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Request, Res, UseGuards } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { LoginDto } from './dto/login.dto';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
-import { Response } from 'express';
-import { RequestType } from 'src/interfaces/types';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
-import { ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
     private readonly usersService: UsersService,
-    private configService: ConfigService,
+    // private configService: ConfigService,
   ) {}
 
   @HttpCode(HttpStatus.CREATED)
@@ -24,10 +21,10 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  async login(@Res() res: Response, @Body() loginDto: LoginDto) {    
+  async login(@Body() loginDto: LoginDto) {    
     const tokens = await this.authService.login(loginDto.email, loginDto.password);    
-    res.cookie('Authorization', `Bearer ${tokens.accessToken}`, { httpOnly: true }); // set 1month
-    res.cookie('Refresh', tokens.refreshToken, { httpOnly: true});
+    // res.cookie('Authorization', `Bearer ${tokens.accessToken}`, { httpOnly: true }); // set 1month
+    // res.cookie('Refresh', tokens.refreshToken, { httpOnly: true});
     console.info("Loggin : ", tokens);
     return tokens;
   }
@@ -35,21 +32,21 @@ export class AuthController {
   @UseGuards(AuthGuard)
   @HttpCode(HttpStatus.OK)
   @Get('logout')
-  logout(@Res() res: Response) {
-    res.clearCookie('Authorization');
-    res.clearCookie('Refresh');
+  logout() {
+    // res.clearCookie('Authorization');
+    // res.clearCookie('Refresh');
   }
 
-  @UseGuards(AuthGuard)
-  @Get('refresh-token')
-  @HttpCode(HttpStatus.OK)
-  async refresh(@Request() req: RequestType, @Res({ passthrough: true }) res: Response) {
-    const refreshToken = req.cookies['Refresh'];    
-    const newTokens = await this.authService.refresh(refreshToken);    
-    res.cookie('Authorization', `Bearer ${newTokens.accessToken}`, { httpOnly: true });
-    res.cookie('Refresh', newTokens.refreshToken, { httpOnly: true });
-    return newTokens;
-  }
+  // @UseGuards(AuthGuard)
+  // @Get('refresh-token')
+  // @HttpCode(HttpStatus.OK)
+  // async refresh(@Request() req: RequestType, @Res({ passthrough: true }) res: Response) {
+  //   const refreshToken = req.cookies['Refresh'];    
+  //   const newTokens = await this.authService.refresh(refreshToken);    
+  //   res.cookie('Authorization', `Bearer ${newTokens.accessToken}`, { httpOnly: true });
+  //   res.cookie('Refresh', newTokens.refreshToken, { httpOnly: true });
+  //   return newTokens;
+  // }
 
   @UseGuards(AuthGuard)
   @Get('check-token')
