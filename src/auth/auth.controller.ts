aa9ca8faafb9f8b43cf989b00532bdf619cdea5b
Laura <laura@upschool.fr>
2024-04-11 17:09:28 +0200

    test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { RequestType } from 'src/interfaces/types';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
@@ -19,10 +21,10 @@ export class AuthController {
   }
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {    
+  async login(@Res({passthrough: true}) res: Response, @Body() loginDto: LoginDto) {    
     const tokens = await this.authService.login(loginDto.email, loginDto.password);    
-    // res.cookie('Authorization', `Bearer ${tokens.accessToken}`, { httpOnly: true }); // set 1month
-    // res.cookie('Refresh', tokens.refreshToken, { httpOnly: true});
+    res.cookie('Authorization', `Bearer ${tokens.accessToken}`, { httpOnly: true }); // set 1month
+    res.cookie('Refresh', tokens.refreshToken, { httpOnly: true});
     console.info("Loggin : ", tokens);
     return tokens;
   }
@@ -30,29 +32,27 @@ export class AuthController {
   @UseGuards(AuthGuard)
   @Get('logout')
   logout() {
-    console.log("Logout");
-    
     // res.clearCookie('Authorization');
     // res.clearCookie('Refresh');
   }
 
-  // @UseGuards(AuthGuard)
-  // @Get('refresh-token')
-  // async refresh(@Request() req: RequestType, @Res({ passthrough: true }) res: Response) {
-  //   const refreshToken = req.cookies['Refresh'];    
-  //   const newTokens = await this.authService.refresh(refreshToken);    
-  //   // res.cookie('Authorization', `Bearer ${newTokens.accessToken}`, { httpOnly: true });
-  //   // res.cookie('Refresh', newTokens.refreshToken, { httpOnly: true });
-  //   return newTokens;
-  // }
+  @UseGuards(AuthGuard)
+  @Get('refresh-token')
+  async refresh(@Request() req: RequestType, @Res({ passthrough: true }) res: Response) {
+    const refreshToken = req.cookies['Refresh'];    
+    const newTokens = await this.authService.refresh(refreshToken);    
+    // res.cookie('Authorization', `Bearer ${newTokens.accessToken}`, { httpOnly: true });
+    // res.cookie('Refresh', newTokens.refreshToken, { httpOnly: true });
+    return newTokens;
+  }
 
-  // @UseGuards(AuthGuard)
-  // @Get('check-token')
-  // checkToken(@Request() req: any) {
-  //   const userId = req.user.sub;
-  //   if (!userId) {
-  //     return false;
-  //   }
-  //   return true;
-  // }
+  @UseGuards(AuthGuard)
+  @Get('check-token')
+  checkToken(@Request() req: any) {
+    const userId = req.user.sub;
+    if (!userId) {
+      return false;
+    }
+    return true;
+  }
 }
